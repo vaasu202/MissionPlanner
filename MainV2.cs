0d3e0c81f754d8ef9837c75b4ace18ab48ec47a5
vaasu202 <vsohee@gmail.com>
2022-03-10 18:32:06 +0530

    'update'

--- MainV2.cs
+++ MainV2.cs
@@ -1127,7 +1127,7 @@ namespace MissionPlanner
             }
 
             MenuArduPilot.Image = new Bitmap(Properties.Resources._0d92fed790a3a70170e61a86db103f399a595c70,
-                (int) (210), 37);
+                (int) (270), 37);
             MenuArduPilot.Width = MenuArduPilot.Image.Width;
 
             if (Program.Logo2 != null)
